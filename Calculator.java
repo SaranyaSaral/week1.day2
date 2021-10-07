ba783813e40c74f33336af7c8b6b7aec615a068e
SaranyaSaral <91775614+SaranyaSaral@users.noreply.github.com>
2021-10-07 17:39:28 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,49 @@
+package week1.day2;
+
+public class Calculator {
+	public int add() {
+		int num1=10;
+		int num2=15;
+		int num3=16;
+		int sum=num1+num2+num3;
+		return sum;
+	}
+	public int sub() {
+		int num1=2;
+		int num2=3;
+		int sum= num1-num2;
+		return sum;
+		
+	}
+	public double mul() {
+		double num1=3;
+		double num2=2;
+	    double sum= num1*num2;
+	    return sum;
+	   
+	}
+	public float divide() {
+        float num1=10;
+        float num2=5;
+        float sum= num1/num2;
+        return sum;
+        
+	}
+	public void getresult() {
+	
+	}
+	public void printnum() {
+	}
+	public void getvalue() {
+	}
+	public void getmark() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+	}
+
+
