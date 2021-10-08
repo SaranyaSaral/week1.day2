5bb404d14a4cf276cd98498c2e38f7be1b20839e
SaranyaSaral <91775614+SaranyaSaral@users.noreply.github.com>
2021-10-08 17:10:56 +0530

    Add files via upload

--- MissingElementInArray.java
+++ MissingElementInArray.java
@@ -9,10 +9,15 @@ int[] arr = {1,2,3,4,7,6,8};
 Arrays.sort(arr);
 for (int i=0; i<arr.length;i++) {
 	System.out.println(arr[i]);
-	if(arr[i]+1!=arr[i+1]) {
-		System.out.println("missing element:"  + arr[i+1]);
+	if(arr[i]!=i+1 ) {
+		i++;
+		System.out.println("missing element:"  + i);
 		break;
+		
 	}
+	
+	
+	
 	
 	}
 
